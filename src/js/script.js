8bf516eb274384ae0a3b7565dabbb872872a45ea
Alekssey Demchenko <ad@pesochin.net>
2023-01-26 22:10:22 +0200

    Portfolio Improvement of the sidebar

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,17 @@
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
-      closeElem = document.querySelector('.menu__close');
+      closeElem = document.querySelector('.menu__close'),
+      closeMenu = document.querySelector('.menu__link');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
 
 closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+closeMenu.addEventListener('click', () => {
     menu.classList.remove('active');
 });
 
