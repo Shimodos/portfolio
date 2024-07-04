b2704e9bd656e07e14fac13225574ebdaed2d8c8
Aleksey Demchenko <ad@pesochin.net>
2024-07-04 16:46:38 +0300

    fix: Portfolio 04.07.24

--- src/js/script.js
+++ src/js/script.js
@@ -1,76 +1,36 @@
+// Гамбургер-меню
 const hamburger = document.querySelector('.hamburger'),
-      menu = document.querySelector('.menu'),
-      closeElem = document.querySelector('.menu__close'),
-      closeMenu = document.querySelector('.menu__list');
+  menu = document.querySelector('.menu'),
+  closeElem = document.querySelector('.menu__close'),
+  closeMenu = document.querySelector('.menu__list');
 
 hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
+  menu.classList.add('active');
 });
 
 closeElem.addEventListener('click', () => {
-    menu.classList.remove('active');
+  menu.classList.remove('active');
 });
 
 closeMenu.addEventListener('click', () => {
-    menu.classList.remove('active');
+  menu.classList.remove('active');
 });
 
+// Заполнение прогресс-баров навыков
 const counters = document.querySelectorAll('.skills__ratings-counter'),
-      lines = document.querySelectorAll('.skills__ratings-line span');
+  lines = document.querySelectorAll('.skills__ratings-line span');
 
 counters.forEach((item, i) => {
-    lines[i].style.width = item.innerHTML;
+  lines[i].style.width = item.innerHTML;
 });
 
-
-
-//  Smooth scroll
-
-// const anchors = document.querySelectorAll('a[href*="#"]')
-
-// for (let anchor of anchors) {
-//   anchor.addEventListener('click', function (e) {
-//     e.preventDefault()
-    
-//     const blockID = anchor.getAttribute('href').substr(1);
-    
-//     document.getElementById(blockID).scrollIntoView({
-//       behavior: 'smooth',
-//       block: 'start'
-//     });
-//   });
-// }
-
-// собираем все якоря; устанавливаем время анимации и количество кадров
-const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
-      animationTime = 300,
-      framesCount = 20;
-
-anchors.forEach(function(item) {
-  // каждому якорю присваиваем обработчик события
-  item.addEventListener('click', function(e) {
-    // убираем стандартное поведение
+// Плавный скроллинг
+document.querySelectorAll('a[href*="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
     e.preventDefault();
-    
-    // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
-    let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
-    
-    // запускаем интервал, в котором
-    let scroller = setInterval(function() {
-      // считаем на сколько скроллить за 1 такт
-      let scrollBy = coordY / framesCount;
-      
-      // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
-      // и дно страницы не достигнуто
-      if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
-        // то скроллим на к-во пикселей, которое соответствует одному такту
-        window.scrollBy(0, scrollBy);
-      } else {
-        // иначе добираемся до элемента и выходим из интервала
-        window.scrollTo(0, coordY);
-        clearInterval(scroller);
-      }
-    // время интервала равняется частному от времени анимации и к-ва кадров
-    }, animationTime / framesCount);
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
   });
 });
